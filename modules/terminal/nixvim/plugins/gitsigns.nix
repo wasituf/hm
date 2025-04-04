a917fac2f8da7a9aa02b94c905f966b70731e4a5
Wasit Faraizi <wasit.allin1@gmail.com>
2025-04-04 11:48:48 +0600

    nixvim: change gitsigns.nvim delay to 1000ms

--- modules/terminal/nixvim/plugins/gitsigns.nix
+++ modules/terminal/nixvim/plugins/gitsigns.nix
@@ -8,7 +8,7 @@
         current_line_blame_formatter = "    @<author>, <author_time:%Y-%m-%d> <summary> ";
         current_line_blame_formatter_nc = "    <author> ";
         current_line_blame_opts = {
-          delay = 10;
+          delay = 1000;
           virt_text = true;
           virt_text_pos = "eol";
         };
