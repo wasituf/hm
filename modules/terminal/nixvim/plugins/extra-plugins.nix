f575843b7d35216345a99fa4a0abfd0140d5cd82
Wasit Faraizi <wasit.allin1@gmail.com>
2025-04-04 18:12:20 +0600

    nixvim: add whitespace-nvim

--- modules/terminal/nixvim/plugins/extra-plugins.nix
+++ modules/terminal/nixvim/plugins/extra-plugins.nix
@@ -4,6 +4,7 @@
     extraPlugins = with pkgs.vimPlugins; [
       eyeliner-nvim
       supermaven-nvim
+      whitespace-nvim
     ];
 
     extraConfigLua = ''
@@ -24,6 +25,16 @@
         disable_keymaps = false;
         ignore_filetypes = { }, -- { cpp = true, } or { "cpp", }
       })
+
+      require('whitespace-nvim').setup({
+        highlight = 'DiffDelete',
+        ignored_filetypes = { 'TelescopePrompt', 'Trouble', 'FzfLua', 'help', 'dashboard' },
+        ignore_terminal = true,
+        return_cursor = true,
+      })
+
+      -- remove trailing whitespace with a keybinding
+      -- vim.keymap.set('n', '<Leader>t', require('whitespace-nvim').trim)
     '';
   };
 }
